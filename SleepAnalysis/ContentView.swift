700c8bbc083d71350c570f17c2121969f62d26ae
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2023-05-08 03:00:49 +0900

    refactor data processing

--- SleepAnalysis/ContentView.swift
+++ SleepAnalysis/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Charts
+import CoreData
 
 struct LineData: Encodable, Identifiable{
     let id = UUID()
@@ -23,9 +24,91 @@ struct LineData: Encodable, Identifiable{
 
 public func formatDate(offset: Double)->Date{
     let date = Date.now.addingTimeInterval(offset)
-//    let dateFormatter = DateFormatter()
-//    let stringVal = dateFormatter.string(from: date)
     return date
+}
+
+struct entry{
+    let sleepStart: Date?
+    let sleepEnd: Date?
+}
+
+func processSleepData(V0: [Double], entries: FetchedResults<Entry>) -> ([[Double]], [(Double, String)], [Double]){
+    
+    @AppStorage("sleep_onset") var sleep_onset: Date = Date.now
+    @AppStorage("work_onset") var work_onset: Date = Date.now
+    @AppStorage("work_offset") var work_offset: Date = Date.now
+    
+    @AppStorage("V0_x") var V0_x: Double = -0.8283
+    @AppStorage("V0_y") var V0_y: Double = 0.8413
+    @AppStorage("V0_n") var V0_n: Double = 0.6758
+    @AppStorage("V0_H") var V0_H: Double = 13.3336
+    
+    var suggestion_pattern = [(Double, String)](repeating: (0.0, "Main Sleep"), count: 12*24*2+10)
+    var sleep_pattern = [Double](repeating: 0.0, count: 12*24*2+10)
+    var y_data = [[Double]](repeating: [0.0], count: 12*24*2+10)
+    
+    @AppStorage("mainSleepStart") var mainSleepStart: Date = Date.now
+    @AppStorage("mainSleepEnd") var mainSleepEnd: Date = Date.now
+    @AppStorage("napSleepStart") var napSleepStart: Date = Date.now
+    @AppStorage("napSleepEnd") var napSleepEnd: Date = Date.now
+    
+    //PCR prediction initial data
+    let startDate = Date.now.addingTimeInterval(-1.0*60*60*24*1)
+    //Sleep Suggestion
+    let step = 1/12.0
+    let V0_suggest = [V0_x, V0_y, V0_n, V0_H]
+
+    let suggest = Sleep_pattern_suggestion(V0: V0_suggest, sleep_onset: Int(sleep_onset.timeIntervalSince(Date.now)/60/5), work_onset: Int(work_onset.timeIntervalSince(Date.now)/60/5), work_offset: Int(work_offset.timeIntervalSince(Date.now)/60/5), step: step)
+    let MainSleep = suggest.CSS
+    let NapSleep = suggest.Nap
+    
+    mainSleepStart = Date.now.addingTimeInterval(Double(MainSleep[0])*5.0*60.0)
+    mainSleepEnd = Date.now.addingTimeInterval(Double(MainSleep[1])*5.0*60.0)
+    napSleepStart = Date.now.addingTimeInterval(Double(NapSleep[0])*5.0*60.0)
+    napSleepEnd = Date.now.addingTimeInterval(Double(NapSleep[1])*5.0*60.0)
+    print("Main sleep: ", mainSleepStart)
+    print("Main sleep: ", mainSleepEnd)
+    print("Nap sleep: ", napSleepStart)
+    print("Nap sleep: ", napSleepEnd)
+    
+    
+    var idx = Int(MainSleep[0])
+    var offset = Int(MainSleep[1])
+    for i in 0..<offset{
+        suggestion_pattern[288+idx+i] = (1.0, "Main Sleep")
+        sleep_pattern[288+idx+i] = 1.0
+    }
+    
+    idx = Int(NapSleep[0])
+    offset = Int(NapSleep[1])
+    for i in 0..<offset{
+        suggestion_pattern[288+idx+i] = (1.0, "Nap")
+        sleep_pattern[288+idx+i] = 1.0
+    }
+    
+    
+    for entry in entries{
+        if entry.sleepStart == nil{
+            continue
+        }
+        if entry.sleepStart! < startDate{
+            continue
+        }else{
+            let startToSleep = entry.sleepStart?.timeIntervalSince(startDate)
+            let sleepToSleep = entry.sleepEnd!.timeIntervalSince(entry.sleepStart!)
+            
+            //fill out the array
+            let idx = Int(startToSleep!/60/5)
+            let offset = Int(sleepToSleep/60/5)
+                        
+            for i in 0..<offset{
+                sleep_pattern[idx+i] = 1.0
+            }
+        }
+    }
+    y_data = pcr_simulation(V0: V0, sleep_pattern: sleep_pattern, step: 5/60.0)
+    
+    return (y_data, suggestion_pattern, sleep_pattern)
 }
 
 struct ContentView: View {
@@ -37,27 +120,16 @@ struct ContentView: View {
     @State var isLoading = false
     @State var lastUpdated = Date.now.addingTimeInterval(-60.0*60.0*3)
     
-    @AppStorage("sleep_onset") var sleep_onset: Date = Date.now
-    @AppStorage("work_onset") var work_onset: Date = Date.now
-    @AppStorage("work_offset") var work_offset: Date = Date.now
     @AppStorage("lastSleep") var lastSleep:Date = Date.now.addingTimeInterval(-1*60.0*60.0*24.0*14.0)
     @AppStorage("needUpdate") var needUpdate:Bool = false
-    
-    @AppStorage("V0_x") var V0_x: Double = -0.8283
-    @AppStorage("V0_y") var V0_y: Double = 0.8413
-    @AppStorage("V0_n") var V0_n: Double = 0.6758
-    @AppStorage("V0_H") var V0_H: Double = 13.3336
     
     @FetchRequest(sortDescriptors: []) var entries: FetchedResults<Entry>
     @FetchRequest(sortDescriptors: []) var V0_cores: FetchedResults<V0_main>
     
-    @State var suggestion_pattern = [(Double, String)](repeating: (0.0, "Main Sleep"), count: 12*24*2+10)
-    @State var sleep_pattern = [Double](repeating: 0.0, count: 12*24*2+10)
-    @State var y_data = [[Double]](repeating: [0.0], count: 12*24*2+10)
-    @State var mainSleepStart = Date.now
-    @State var mainSleepEnd = Date.now
-    @State var napSleepStart = Date.now
-    @State var napSleepEnd = Date.now
+    @AppStorage("mainSleepStart") var mainSleepStart: Date = Date.now
+    @AppStorage("mainSleepEnd") var mainSleepEnd: Date = Date.now
+    @AppStorage("napSleepStart") var napSleepStart: Date = Date.now
+    @AppStorage("napSleepEnd") var napSleepEnd: Date = Date.now
     
     var body: some View {
         
@@ -72,12 +144,6 @@ struct ContentView: View {
                             .background(.white)
                             
                         VStack (spacing: 40){
-//                            AlertView(tabSelection: $tabSelection)
-//                                .padding()
-//                                .background(.white)
-//                                .previewLayout(.fixed(width: 400, height: 60))
-//                                .cornerRadius(15)
-//                                .onTapGesture{print("you clicked alertView")}
                             SleepTimeView(tabSelection: $tabSelection, mainSleepStart: $mainSleepStart, mainSleepEnd: $mainSleepEnd, napSleepStart: $napSleepStart, napSleepEnd: $napSleepEnd)
                                 .padding()
                                 .background(.white)
@@ -92,42 +158,6 @@ struct ContentView: View {
                                 .cornerRadius(15)
                                 .frame(height: geometry.size.height/3)
                                 .onTapGesture{print("you clicked graphView")}
-                            //                List{
-                            //                Chart {
-                            //                    ForEach(SleepData){
-                            //                        LineMark(
-                            //                            x: .value("x1", $0.x),
-                            //                            y: .value("y1", $0.y),
-                            //                            series: .value("sleep", "s")
-                            //                        ).foregroundStyle(by: .value("Category", $0.Category))
-                            //                        AreaMark(
-                            //                            x: .value("x1", $0.x),
-                            //                            y: .value("y1", $0.y)
-                            //                        ).foregroundStyle(.blue)
-                            //                    }
-                            //                    ForEach(AwarenessData){
-                            //                        LineMark(
-                            //                            x: .value("x", $0.x),
-                            //                            y: .value("y", $0.y),
-                            //                            series: .value("awareness", "a")
-                            //                        ).interpolationMethod(.catmullRom)
-                            //                            .foregroundStyle(by: .value("Category", $0.Category))
-                            //                    }
-                            //
-                            //                }.frame(height: geometry.size.height/3)
-                            //                //                }.refreshable {
-                            //                //                    print("something")
-                            //                //                }
-                            //                Chart(SleepSuggestionData) {
-                            //                    LineMark(
-                            //                        x: .value("x", $0.x),
-                            //                        y: .value("y", $0.y)
-                            //                    ).foregroundStyle(by: .value("Category", $0.Category))
-                            //                    AreaMark(
-                            //                        x: .value("x", $0.x),
-                            //                        y: .value("y", $0.y)
-                            //                    ).foregroundStyle(by: .value("Category", $0.Category))
-                            //                }.frame(height: geometry.size.height/3)
                         }.padding()
                     }
                     .onAppear(){
@@ -138,6 +168,7 @@ struct ContentView: View {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 2){
                                 needUpdate = false
                                 lastUpdated = Date.now
+                                
                                 //PCR prediction initial data
                                 let startDate = Date.now.addingTimeInterval(-1.0*60*60*24*1)
                                 var V0 =  [-0.8590, -0.6837, 0.1140, 14.2133] //initial condition
@@ -150,66 +181,7 @@ struct ContentView: View {
                                     }
                                     managedObjectContext.delete(v)
                                 }
-                                
-                                //Sleep Suggestion
-                                let step = 1/12.0
-                                let V0_suggest = [V0_x, V0_y, V0_n, V0_H]
-                                let suggest = Sleep_pattern_suggestion(V0: V0_suggest, sleep_onset: Int(sleep_onset.timeIntervalSince(Date.now)/60/5), work_onset: Int(work_onset.timeIntervalSince(Date.now)/60/5), work_offset: Int(work_offset.timeIntervalSince(Date.now)/60/5), step: step)
-                                let MainSleep = suggest.CSS
-                                let NapSleep = suggest.Nap
-                                
-                                //debug
-                                mainSleepStart = Date.now.addingTimeInterval(Double(MainSleep[0])*5.0*60.0)
-                                mainSleepEnd = Date.now.addingTimeInterval(Double(MainSleep[1])*5.0*60.0)
-                                napSleepStart = Date.now.addingTimeInterval(Double(NapSleep[0])*5.0*60.0)
-                                napSleepEnd = Date.now.addingTimeInterval(Double(NapSleep[1])*5.0*60.0)
-//                                print("Main sleep: ", mainSleepStart)
-//                                print("Main sleep: ", mainSleepEnd)
-//                                print("Nap sleep: ", napSleepStart)
-//                                print("Nap sleep: ", napSleepEnd)
-                                
-                                
-                                var idx = Int(MainSleep[0])
-                                var offset = Int(MainSleep[1])
-                                for i in 0..<offset{
-                                    suggestion_pattern[288+idx+i] = (1.0, "Main Sleep")
-                                    sleep_pattern[288+idx+i] = 1.0
-                                }
-                                
-                                idx = Int(NapSleep[0])
-                                offset = Int(NapSleep[1])
-                                for i in 0..<offset{
-                                    suggestion_pattern[288+idx+i] = (1.0, "Nap")
-                                    sleep_pattern[288+idx+i] = 1.0
-                                }
-                                
-                                
-                                for entry in entries{
-                                    if entry.sleepStart == nil{
-                                        continue
-                                    }
-                                    if entry.sleepStart! < startDate{
-                                        //                        print(entry.sleepStart!)
-                                        continue
-                                    }else{
-                                        //distance from startDate:
-                                        let startToSleep = entry.sleepStart?.timeIntervalSince(startDate)
-                                        let sleepToSleep = entry.sleepEnd!.timeIntervalSince(entry.sleepStart!)
-                                        
-                                        //                        print(String(startToSleep!), String(sleepToSleep))
-                                        //fill out the array
-                                        let idx = Int(startToSleep!/60/5)
-                                        let offset = Int(sleepToSleep/60/5)
-                                        
-                                        //                        print(idx, offset)
-                                        
-                                        for i in 0..<offset{
-                                            sleep_pattern[idx+i] = 1.0
-                                        }
-                                    }
-                                }
-                                y_data = pcr_simulation(V0: V0, sleep_pattern: sleep_pattern, step: 5/60.0)
-                                //                let start = DispatchTime.now()
+                                let (y_data, suggestion_pattern, sleep_pattern) = processSleepData(V0: V0, entries: entries)
                                 let yesterday = Date.now.addingTimeInterval(60.0*60.0*24.0)
                                 for x in 0...575{
                                     let V0_core = V0_main(context: managedObjectContext)
